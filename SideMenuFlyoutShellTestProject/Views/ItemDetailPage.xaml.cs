236e7711f95311978d2c735f04512a09142f730e
Nadiia Zhurba / Berdnyk <nadiiab@y-tree.com>
2020-10-08 12:45:23 +0300

    * .gitignore: Initial Commit
    * SideMenuFlyoutShellTestProject.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * AppShell.xaml:
    * Info.plist:
    * Item.cs:
    * AssemblyInfo.cs:
    * AppShell.xaml.cs:
    * AppDelegate.cs:
    * GettingStarted.txt:
    * LoginPage.xaml:
    * ItemsPage.xaml:
    * AboutPage.xaml:
    * IDataStore.cs:
    * NewItemPage.xaml:
    * Entitlements.plist:
    * ItemsPage.xaml.cs:
    * LoginPage.xaml.cs:
    * MainActivity.cs:
    * AboutPage.xaml.cs:
    * MockDataStore.cs:
    * ItemDetailPage.xaml:
    * Default.png:
    * NewItemPage.xaml.cs:
    * tab_feed.png:
    * BaseViewModel.cs:
    * tab_about.png:
    * ItemDetailPage.xaml.cs:
    * LoginViewModel.cs:
    * ItemsViewModel.cs:
    * AboutViewModel.cs:
    * Default@2x.png:
    * tab_feed@3x.png:
    * tab_feed@2x.png:
    * NewItemViewModel.cs:
    * xamarin_logo.png:
    * tab_about@3x.png:
    * tab_about@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * ItemDetailViewModel.cs:
    * xamarin_logo@3x.png:
    * Default-568h@2x.png:
    * xamarin_logo@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * Tabbar.xml:
    * styles.xml:
    * colors.xml:
    * Toolbar.xml:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * SideMenuFlyoutShellTestProject.csproj:
    * Default-Portrait@2x.png:
    * AndroidManifest.xml:
    * icon.png:
    * Resource.designer.cs:
    * icon.png:
    * tab_feed.png:
    * icon.png:
    * icon.png:
    * tab_about.png:
    * icon.png:
    * xamarin_logo.png:
    * icon.xml:
    * SideMenuFlyoutShellTestProject.iOS.csproj:
    * Icon80.png:
    * Icon58.png:
    * Icon60.png:
    * Icon29.png:
    * Icon76.png:
    * Icon87.png:
    * Icon20.png:
    * Icon40.png:
    * icon_round.xml:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * SideMenuFlyoutShellTestProject.Android.csproj:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ SideMenuFlyoutShellTestProject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using SideMenuFlyoutShellTestProject.ViewModels;
+
+namespace SideMenuFlyoutShellTestProject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
