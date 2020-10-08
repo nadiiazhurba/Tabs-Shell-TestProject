fa928eef88e6dd08c1f13b45debe5431c56047bb
Nadiia Zhurba / Berdnyk <nadiiab@y-tree.com>
2020-10-08 16:48:44 +0300

    * AndroidManifest.xml: Started implementation of flyout menu items
    * ItemsPage.xaml:
    * LoginPage.xaml:
    * IDataStore.cs:
    * NewItemPage.xaml:
    * title.png:
    * ItemsPage.xaml.cs:
    * LoginPage.xaml.cs:
    * NewItemPage.xaml.cs:
    * MockDataStore.cs:
    * ItemDetailPage.xaml:
    * BaseViewModel.cs:
    * ItemsViewModel.cs:
    * LoginViewModel.cs:
    * ItemDetailPage.xaml.cs:
    * SolidPrinciplePage.xaml:
    * NewItemViewModel.cs:
    * SolidPrinciplePage.xaml.cs:
    * ItemDetailViewModel.cs:
    * title.png:
    * Resource.designer.cs:

---
+++ SideMenuFlyoutShellTestProject/Views/SolidPrinciplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SideMenuFlyoutShellTestProject.Views
+{
+    public partial class SolidPrinciplePage : ContentPage
+    {
+        public SolidPrinciplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
