ba303ea81a156f99cff6d782688d6f15027cbc54
Nadiia Zhurba / Berdnyk <nadiiab@y-tree.com>
2020-10-09 15:09:27 +0300

    * AppShell.xaml: Implemented UI with combination
    * AppShell.xaml.cs:

--- SideMenuFlyoutShellTestProject/AppShell.xaml.cs
+++ SideMenuFlyoutShellTestProject/AppShell.xaml.cs
@@ -1,13 +1,27 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace SideMenuFlyoutShellTestProject
 {
     public partial class AppShell : Shell
     {
+        private readonly Random rand = new Random();
+        public Dictionary<string, Type> Routes { get; } = new Dictionary<string, Type>();
+
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoutes();
             BindingContext = this;
+        }
+
+        private void RegisterRoutes()
+        {
+            foreach (var item in Routes)
+            {
+                Routing.RegisterRoute(item.Key, item.Value);
+            }
         }
     }
 }
